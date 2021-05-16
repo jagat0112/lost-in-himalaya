2b094ed3842ba799095b10b74b5ef058e45bee1e
Jagat Pradhan <jagat.pradhan0112@gmail.com>
2021-05-16 23:30:10 +0545

    footer made

--- js/app.js
+++ js/app.js
@@ -5,3 +5,61 @@ collabsible.forEach((c) => {
     this.classList.toggle("collapsible--active");
   });
 });
+
+// const person = {
+//   name: "norgen",
+//   hobbies: ["a", "b"],
+//   age: 10,
+//   change(name) {
+//     this.name = name;
+//   },
+//   hob: [1, 2, 3],
+
+//   greet() {
+//     this.hobbies.forEach(
+//       function (hob) {
+//         console.log(hob);
+//       }.bind(this)
+//     );
+//   },
+// };
+
+// person.greet();
+
+// callPerson();
+
+// function CallingAnother(name) {
+//   this.name = name;
+//   this.calling = () => {
+//     console.log(this.name);
+//   };
+// }
+
+// const action = new CallingAnother("jagat shrestha");
+// action.calling();
+
+// const callPerson = (nam) => {
+//   return {
+//     nam,
+//     calling: () => {
+//       console.log(nam);
+//     },
+//   };
+// };
+// const callAnu = callPerson("Anu");
+// callAnu.calling();
+
+// function CallPerson2(nam) {
+//   this.nam = nam;
+//   this.calling = () => {
+//     console.log(nam);
+//   };
+//   this.greet = () => {
+//     console.log(`Hello ${name}`);
+//   };
+// }
+// const calls = new CallPerson2("krishan pradhan");
+// const calls1 = new CallPerson2("mami");
+
+// calls.calling();
+// calls.greet();
